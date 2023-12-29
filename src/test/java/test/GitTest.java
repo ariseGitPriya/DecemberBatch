4dcecb55fe46dce5ab65084eb193190fe67eaed9
ariseGitPriya <priyankasky90@gmail.com>
2023-12-29 10:49:29 +0530

    New method created in GitTest Class

---
+++ src/test/java/test/GitTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+
+import pojo.Brawser;
+
+public class GitTest {
+	
+	public void demo()
+	{
+		WebDriver driver=Brawser.chrome("https://www.facebook.com/");
+	}
+
+}
